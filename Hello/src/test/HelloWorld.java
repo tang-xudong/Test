48171802d23bf9d5b858e257d5e2912b9880daf4
000 <000@000-PC>
2020-01-03 13:35:37 +0800

    s

--- Hello/src/test/HelloWorld.java
+++ Hello/src/test/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 
 		String str = "Hello World";
 		System.out.println(10);
-
+		String youString="如果我是dj你会爱我吗";
 		int a =20;
 		System.out.println(10+a);
 		System.out.println("aaaaaaaaaaaaaaaaaaaaaa");
